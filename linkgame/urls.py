775403aeb2f27f0b2d11318c9818599fe5009c3b
Nichole <kntreadway@gmail.com>
2010-04-26 23:57:05 -0400

    Updated game template to check when the game has been won

--- linkgame/urls.py
+++ linkgame/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls.defaults import *
+from django.contrib.auth.views import login, logout
 from linkgame.views import new_game
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-                       (r'^$', new_game),
+                       (r'^newgame/$', new_game),
+                       (r'^accounts/login/$', 'django.contrib.auth.views.login'),
+                       (r'^accounts/logout/$', logout),
+
     # Example:
     # (r'^linkgame/', include('linkgame.foo.urls')),
 
--- linkgame/views.py
+++ linkgame/views.py
@@ -8,18 +8,25 @@ import random
 
 def new_game(request):
     print "Getting concepts"
-    #concept1, concept2 = get_random_concepts('en')
+
     relations =  get_all_relations('en')
-    print relations
-    concept1, concept2 = ['dog', 'cat']
+    concept1, concept2 = get_random_concepts('en')
     return render_to_response('ui.html', {'concept1': concept1, 'concept2': concept2, 'relationsList': relations})
 
-
-
 #concepts = get_random_concepts(language, 10)
+def get_random_concepts(lang):
 
+    concept1 = random.choice(random_cs)
+    while True:
+        concept2 = random.choice(random_cs)
+        if concept1 != concept2:
+            break
+    return (concept1, concept2)
+    
+    #return get_random_from_CN(lang) #Uncomment to get random concepts from ConceptNet
+    
 #copied from commonsense.queries
-def get_random_concepts(lang, num=2):
+def get_random_from_CN(lang, num=2):
    # concepts = Concept.objects.filter(language=lang).filter(words = 1).order_by('?')
    assertions = RawAssertion.objects.filter(score__gt=2, language=lang).select_related('surface1').order_by('?')
 
@@ -52,6 +59,7 @@ def get_all_relations(lang):
 #print get_all_relations('en')
 
 
+random_cs = ["panda", "pony", "coffee", "notebook", "computer", "pen", "clown", "giraffe", "elephant"]
 
 
 
